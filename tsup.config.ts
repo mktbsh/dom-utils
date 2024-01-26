dc2b7bbbcfeeed7d2a77114a6186b0605a9479e4
mktbsh <t93.hasebe@gmail.com>
2024-01-26 19:01:29 +0900

    add: tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  target: "es2017",
+  format: ["cjs", "esm"],
+  clean: true,
+  dts: true,
+  treeshake: true,
+  sourcemap: true,
+});
